8c84ad89773911f60a60d5252eb04c585e15ccfb
Amin Pournaghi <aminpournaghii5@gmail.com>
2024-11-24 16:44:14 +0330

    type of row of exercises

--- src/Type/Type.ts
+++ src/Type/Type.ts
@@ -1,18 +1,26 @@
 export interface IExercise {
-    _id: string ;
-    ID: number;
-    NameFarsi: string;
-    Name?: string;
-    Target?: string;
-    TargetFarsi?: string;
-    Synergist?: string;
-    SynergistFarsi?: string;
-    Equipment?: string;
-    Type?: string;
-    Gender?: string;
-    Abzar?: string;
-    TypeFarsi?: string;
-    Body?: string;
-    setsReps?: any ; 
-  }
-  
+  _id: string;
+  ID: number;
+  NameFarsi: string;
+  Name?: string;
+  Target?: string;
+  TargetFarsi?: string;
+  Synergist?: string;
+  SynergistFarsi?: string;
+  Equipment?: string;
+  Type?: string;
+  Gender?: string;
+  Abzar?: string;
+  TypeFarsi?: string;
+  Body?: string;
+  setsReps?: any;
+  rows: row[];
+}
+
+type Set = number | "گرم کردن" | "تا واماندگی" | "دراپ ست";
+type Reps = number | { min: number; max: number };
+
+interface row {
+  set: Set;
+  reps: Reps;
+}
